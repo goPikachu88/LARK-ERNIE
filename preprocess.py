833da68a52d504d8aa3c93306e42b230651e4e43
ardellelee <ardellelee@outlook.com>
2019-07-24 11:17:06 +0800

    new NER and Relation tansformer

--- preprocess.py
+++ preprocess.py
@@ -5,7 +5,7 @@ Description:
 Pre-process datasets to satisfy the format of ERNIE.
 
 Todo:
-Label_map is defined but actually not used for pre-processing step. Remove later.
+
 
 '''
 
@@ -19,62 +19,8 @@ from itertools import combinations, permutations
 from tokenization import FullTokenizer
 
 
-class BaseTransformer(object):
-    # def __init__(self, label_map):
-    #     self.label_map = label_map
-
-    def transform(self, data):
-        '''
-        Unpack json data and transform to target instances.
-        '''
-        # print("Json instances: %d" % len(data))
-        #
-        # # unpack spo_list
-        # unpacked = []
-        # for d in data:
-        #     for spo in d['spo_list']:
-        #         unpacked.append(dict(text=d['text'],
-        #                              relation=spo['predicate'],
-        #                              subj=spo['subject'],
-        #                              obj=spo['object'],
-        #                              # subj_type=self.label_map[spo['subject_type']],
-        #                              # obj_type=self.label_map[spo['object_type']],
-        #                              subj_type=spo['subject_type'],
-        #                              obj_type=spo['object_type'],
-        #                              )
-        #                         )
-        # print('Unpacked SPO: %d' % len(unpacked))
-        # random.Random(4).shuffle(unpacked)
-        #
-        # # return [self._transform_one(d) for d in unpacked]
-        # transformed = []
-        # error_idx = []
-        # for i, d in enumerate(unpacked):
-        #     if i % 10000 == 0:
-        #         print('Processed %d unpacked examples.' % i)
-        #     try:
-        #         o = self._transform_one(d)
-        #     except:
-        #         o = {}          # maybe more robust to skip the instance than to return {}?
-        #         error_idx.append(i)
-        #     transformed.append(o)
-        # print('%d examples cannot be transformed.' % len(error_idx))
-        # print(error_idx)
-        # return transformed
-        raise NotImplementedError('Method not implemented.')
-
-
-    def _transform_one(self, instance):
-        '''transform an unpacked spo instance from
-            ['text'], ['subj'], ['obj'], ['relation'], ['subj_type'], ['obj_type']
-        to
-            ['text_a'], ['label']
-        '''
-        raise NotImplementedError('Method not implemenetd.')
-
-
 class RelationTransformer(object):
-    def __init__(self, relation_mask='[MASK]', ordered=False, none_label='no_relation', **kwargs):
+    def __init__(self, relation_mask='[MASK]', ordered=True, none_label='no_relation', **kwargs):
         super().__init__(**kwargs)
         self.relation_mask = relation_mask
         self.ordered = ordered
@@ -96,24 +42,25 @@ class RelationTransformer(object):
 
         entity_list = instance.get('entity_list', [])
         relations = []
-        for e1, e2 in combination_func(entity_list):
-            print(e1['text'], e2['text'])
+        for subj, obj in combination_func(entity_list):
             text = instance.get('text', None)
-            text = text.replace(e1['text'], self.relation_mask)
-            text = text.replace(e2['text'], self.relation_mask)
+            text = text.replace(subj['text'], "%s%s" % (self.relation_mask, self.relation_mask))
+            text = text.replace(obj['text'], self.relation_mask)
 
+            r = None
             for spo in instance.get('spo_list', []):
-                match = e1['text'] == spo['subject'] and e2['text'] == spo['object']
-                reverse_match = e1['text'] == spo['object'] and e2['text'] == spo['subject']
+                match = subj['text'] == spo['subject'] and obj['text'] == spo['object']
 
-                if match or reverse_match:
+                if match:
                     r = dict(text_a = text,
                              label = spo['predicate'])
-                else:
-                    r = dict(text_a=text,
-                             label=self.none_label)
-                relations.append(r)
+                    break
             # end for
+            if not r:
+                r = dict(text_a=text,
+                         label=self.none_label)
+            relations.append(r)
+            # print(subj['text'], obj['text'], r['label'])
         # end for
 
         return relations
@@ -130,10 +77,11 @@ class RelationTransformer(object):
         for i, d in enumerate(instances):
             if i % 10000 == 0:
                 print('Processed %d documents.' % i)
-                relations += self._transform_one(d)
-        print('Relation instances (all): %d' % len(relations))
+
+            relations += self._transform_one(d)
 
         # count positive percent
+        print('Relation instances (all): %d' % len(relations))
         n_pos = len([r for r in relations if r['label'] != self.none_label])
         print('Relation instances (positive): %d (%.2f%%)' % (n_pos, n_pos/len(relations)*100))
 
@@ -142,11 +90,16 @@ class RelationTransformer(object):
         return relations
 
 
-class NERTransformer(BaseTransformer):
-    def __init__(self, sep=u"", **kwargs):
-        super().__init__(**kwargs)
+class NERTransformer(object):
+    '''
+    Input: a dict with ['text'], ['spo_list'], ['docid'] and ['entity_list']
+    Output: tsv file with ['text_a'], ['labels'], tokens separated by u"". Keep the doc order as in json.
+
+    Todo: add docid?
+    '''
+    def __init__(self, sep=u"", do_lower_case=True):
         self.tokenizer = FullTokenizer(vocab_file='config/vocab.txt',
-                                       do_lower_case=True)
+                                       do_lower_case=do_lower_case)
         self.sep = sep
 
     def _transform_one(self, instance):
@@ -154,32 +107,29 @@ class NERTransformer(BaseTransformer):
         Tokenize a piece of text and generate a sequence of NER labels for tokens.
         '''
         text_tokens = self.tokenizer.tokenize(instance['text'])
-        subj_tokens = self.tokenizer.tokenize(instance['subj'])
-        obj_tokens = self.tokenizer.tokenize(instance['obj'])
         labels = ["O"] * len(text_tokens)
 
-        # Find entity boundaries
-        subj_start, subj_end = self._find_sublist_boundary(subj_tokens, text_tokens)
-        obj_start, obj_end = self._find_sublist_boundary(obj_tokens, text_tokens)
+        entities = instance.get('entity_list', [])
+        n_overlap = 0
+        for e in entities:
+            e_tokens = self.tokenizer.tokenize(e['text'])
+            try:
+                e_start, e_end = self._find_sublist_boundary(e_tokens, text_tokens)
+            except:
+                continue
 
-        # Add entity BIO labels
-        # 65 labels
-        # labels[subj_start] = 'B-%s-subj' % instance['subj_type']
-        # labels[subj_start+1 : subj_end+1] = ['I-%s-subj' % instance['subj_type']] * (len(subj_tokens)-1)
-        # labels[obj_start] = 'B-%s-obj' % instance['obj_type']
-        # labels[obj_start+1 : obj_end+1] = ['I-%s-obj' % instance['obj_type']] * (len(obj_tokens)-1)
+            # if the span already labelled, skip
+            if len(set(labels[e_start : e_end+1])) > 1:
+                # print('Overlap.')
+                n_overlap += 1
+                continue
 
-        # 57 labels
-        labels[subj_start] = 'B-%s' % instance['subj_type']
-        labels[subj_start+1 : subj_end+1] = ['I-%s' % instance['subj_type']] * (len(subj_tokens)-1)
-        labels[obj_start] = 'B-%s' % instance['obj_type']
-        labels[obj_start+1 : obj_end+1] = ['I-%s' % instance['obj_type']] * (len(obj_tokens)-1)
+            # Add entity BIO labels (57 labels)
+            labels[e_start] = 'B-%s' % e['type']
+            labels[e_start+1 : e_end+1] = ['I-%s' % e['type']] * (len(e_tokens)-1)
+            # print('entity:', text_tokens[e_start:e_end+1], labels[e_start:e_end+1])
 
-        # print('subj:', text_tokens[subj_start:subj_end+1], labels[subj_start:subj_end+1])
-        # print('obj:', text_tokens[obj_start:obj_end+1], labels[obj_start:obj_end+1])
-
-        return dict(text_a=self.sep.join(text_tokens),
-                    label=self.sep.join(labels))
+        return n_overlap, dict(text_a=self.sep.join(text_tokens), label=self.sep.join(labels))
 
     def _find_sublist_boundary(self, sublist, full_list):
         '''
@@ -190,6 +140,24 @@ class NERTransformer(BaseTransformer):
                 return (start, start+len(sublist)-1)
     # end def
 
+    def transform(self, instances):
+        '''
+        Transform a list of json instances (documents) into a list of NER instances.
+        '''
+        print("Json instances: %d" % len(instances))
+
+        transformed = []
+        n_overlap = 0
+        for i, d in enumerate(instances):
+            if i % 10000 == 0:
+                print('Processed %d documents.' % i)
+
+            n_overlap += self._transform_one(d)[0]
+            transformed.append(self._transform_one(d)[1])
+
+        print('Total overlapped entities: %d' % n_overlap)
+
+        return transformed
 
 
 def transform_json(instances):
@@ -235,16 +203,15 @@ def arg_parse():
 def main():
     args = arg_parse()
 
-    data_original = [json.loads(line) for line in args.data][:10]
+    data_original = [json.loads(line) for line in args.data]
     instances = transform_json(data_original)
 
-    transformer = RelationTransformer()
-    # transformer = NERTransformer(label_map = None)
+    # transformer = RelationTransformer()
+    transformer = NERTransformer()
 
     transformed = transformer.transform(instances)
-    pprint(transformed)
-    # write2tsv(f = args.output, unpacked = transformed)
-
+    # pprint(transformed)
+    write2tsv(f = args.output, unpacked = transformed)
 
 
 if __name__ == '__main__':
